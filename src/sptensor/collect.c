75aff2ed1d271aa43cbfac62e738cbb3be31eb89
Star Brilliant <m13253@hotmail.com>
2016-07-08 02:14:37 +0800

    collect.c

--- src/sptensor/collect.c
+++ src/sptensor/collect.c
@@ -1,6 +1,13 @@
 #include <SpTOL.h>
 #include "sptensor.h"
 
+/**
+ * Collect zeros from a sparse tensor
+ *
+ * This function finds zero values from the sparse tensor,
+ * move them to the end of the array, then truncate the array.
+ * Orders are not kept, call sptSparseTensorSortIndex after this.
+ */
 void spt_SparseTensorCollectZeros(sptSparseTensor *tsr) {
     size_t i =  0;
     size_t nnz = tsr->nnz;
