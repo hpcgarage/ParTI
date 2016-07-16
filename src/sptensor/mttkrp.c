a74e8a14881d6fda77a2062e348fb9016374adcb
fruitfly1026 <fruitfly1026@gmail.com>
2016-07-16 10:56:21 -0400

    MTTKRP added

---
+++ src/sptensor/mttkrp.c
@@ -0,0 +1,57 @@
+#include <SpTOL.h>
+#include "sptensor.h"
+
+void sptMTTKRP(sptSparseTensor cosnt * const X, 
+	sptMatrix ** const mats, 	// mats[nmodes] as temporary space.
+	size_t const * const mats_order,
+	size_t const mode,
+	sptScalar * const scratch) {
+
+	size_t const nmodes = X->nmodes;
+	size_t const nnz = X->nnz;
+	size_t const * const ndims = X->ndims;
+	sptScalar const * const vals = X->values;
+	size_t const nmats = nmodes - 1;
+
+	/* Check the mats. */
+	for(size_t i=0; i<nmodes; ++i) {
+		assert(mats[i]->ncols == mats[nmodes]->ncols);
+		assert(mats[i]->nrows == ndims[i]);
+	}
+
+	size_t const I = mats[mode]->nrows;
+	size_t const R = mats[mode]->ncols;
+	size_t cosnt * const mode_ind = X->inds[mode];
+	sptMatrix * const M = mats[nmodes];
+	sptScalar * const mvals = M->vals;
+	memset(mvals, 0, I*R*sizeof(sptScalar));
+
+	for(size_t x=0; x<nnz; ++x) {
+
+		size_t times_mat_index = mats_order[0];
+		sptMatrix * times_mat = mats[times_mat_index];
+		size_t * times_inds = X->inds[times_mat_index];
+		size_t tmp_i = times_inds[x];
+		for(size_t r=0; r<R; ++r) {
+			scratch[r] = times_mat->values[tmp_i * R + r];
+		}
+
+		for(size_t i=1; i<nmats; ++i) {
+			times_mat_index = mats_order[i];
+			times_mat = mats[times_mat_index];
+			times_inds = X=>inds[times_mat_index];
+			tmp_i = times_inds[x];
+
+			for(size_t r=0; r<R; ++r) {
+				scratch[r] += times_mat->values[tmp_i * R + r];
+			}
+		}
+
+		sptScalar const entry = vals[x];
+		size_t const mode_i = mode_ind[x];
+		for(size_t r=0; r<R; ++r) {
+			mvals[mode_i * R + r] += entry * scratch[r];
+		}
+	}
+
+}
