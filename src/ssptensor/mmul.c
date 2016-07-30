fd804cbc0fbf6b4b132269ad5350327cf839fc67
Star Brilliant <m13253@hotmail.com>
2016-07-31 02:24:56 +0800

    Copy out TTM as semi-sparse

---
+++ src/ssptensor/mmul.c
@@ -0,0 +1,10 @@
+#include <SpTOL.h>
+#include <stdlib.h>
+
+/* jli: (TODO) Change to a sparse tensor times a dense matrix.
+    Output a "semi-sparse" tensor in the timing mode.
+    This function can be kept for the future. */
+/* sb: Oh, what a long name */
+int sptSparseTensorMulMatrixAsSemiSparseTensor(sptSemiSparseTensor *Y, const sptSparseTensor *X, const sptMatrix *U, size_t mode) {
+    return 0;
+}
