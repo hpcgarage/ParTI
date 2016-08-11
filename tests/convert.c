1ebb7470cd86795528afaf02bc99e12c06eac430
Star Brilliant <m13253@hotmail.com>
2016-08-12 03:39:59 +0800

    Test: add mode option & fix memory leak

--- tests/convert.c
+++ tests/convert.c
@@ -5,14 +5,17 @@
 
 int main(int argc, char const *argv[]) {
     FILE *fi, *fo;
+    size_t mode = 0;
     sptSparseTensor a;
     sptSemiSparseTensor b;
     sptSparseTensor c;
 
-    if(argc != 3) {
-        printf("Usage %s in out\n\n", argv[0]);
+    if(argc != 4) {
+        printf("Usage %s in out mode\n\n", argv[0]);
         return 1;
     }
+
+    sscanf(argv[3], "%zu", &mode);
 
     fi = fopen(argv[1], "r");
     assert(fi);
@@ -20,11 +23,15 @@ int main(int argc, char const *argv[]) {
     fclose(fi);
 
     assert(sptSparseTensorToSemiSparseTensor(&b, &a, 0) == 0);
+    sptFreeSparseTensor(&a);
     assert(sptSemiSparseTensorToSparseTensor(&c, &b) == 0);
+    sptFreeSemiSparseTensor(&b);
 
     fo = fopen(argv[2], "w");
     assert(sptDumpSparseTensor(&c, fo) == 0);
     fclose(fo);
+
+    sptFreeSparseTensor(&c);
 
     return 0;
 }
