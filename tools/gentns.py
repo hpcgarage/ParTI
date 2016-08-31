e24fa1d126f4fd037baa898335a0d0de732567af
Star Brilliant <m13253@hotmail.com>
2016-09-01 04:47:10 +0800

    Add gentns.py, a tensor generator

--- tools/gentns.py
+++ tools/gentns.py
@@ -6,9 +6,7 @@ import sys
 
 
 if sys.version_info < (3,):
-    iter_range, list_range = xrange, range
-else:
-    iter_range, list_range = range, lambda stop: list(range(stop))
+    range = xrange
 
 
 def randround(x):
@@ -16,6 +14,18 @@ def randround(x):
     frac_part = x - int_part
     return int(math.ceil(x) if random.random() < frac_part else int_part)
 
+
+def human_size(nbytes):
+    if nbytes < 1024000:
+        if nbytes < 1000:
+            return '%d bytes' % nbytes
+        else:
+            return '%.1f KiB' % (nbytes / 1024.0)
+    else:
+        if nbytes < 1048576000:
+            return '%.1f MiB' % (nbytes / 1048576.0)
+        else:
+            return '%.1f GiB' % (nbytes / 1073741824.0)
 
 def main(argv):
     if len(argv) < 2:
@@ -41,7 +51,7 @@ def main(argv):
     ndims = len(dims)
 
     nnz = 1
-    for i in iter_range(ndims):
+    for i in range(ndims):
         nnz *= rates[i] * dims[i]
     print('%d non-zero elements estimated.' % round(nnz))
     written = 0
@@ -50,37 +60,41 @@ def main(argv):
     f = open(output, 'w')
     f.write('%d\n' % ndims)
     f.write('\t'.join(map(str, dims)))
+    f.write('\n')
 
     inds = [None] * ndims
     ptrs = [0] * ndims
-    for i in iter_range(ndims):
+    for i in range(ndims):
         if rates[i] == 1:
-            inds[i] = list_range(dims[i])
+            inds[i] = range(dims[i])
         else:
-            inds[i] = random.sample(iter_range(dims[i]), randround(rates[i] * dims[i]))
+            inds[i] = random.sample(range(dims[i]), randround(rates[i] * dims[i]))
             inds[i].sort()
+
     while ptrs[0] != len(inds[0]):
-        for i in iter_range(ndims):
+        for i in range(ndims):
             f.write('%d\t' % (inds[i][ptrs[i]]+1))
         f.write('% .16f\n' % random.gauss(0, 1))
         ptrs[ndims-1] += 1
+
         written += 1
         if nnz != 0:
             new_percent = int(written * 100.0 / nnz)
             if new_percent < 100 and new_percent != percent:
                 percent = new_percent
-                print('%3d%% completed, %d generated, %.1f MiB written.' % (percent, written, f.tell() / 1048576.0))
-        for i in iter_range(ndims-1, 0, -1):
+                print('%3d%% completed, %d generated, %s written.' % (percent, written, human_size(f.tell())))
+
+        for i in range(ndims-1, 0, -1):
             if ptrs[i] == len(inds[i]):
                 if rates[i] == 1:
-                    inds[i] = list_range(dims[i])
+                    inds[i] = range(dims[i])
                 else:
-                    inds[i] = random.sample(iter_range(dims[i]), randround(rates[i] * dims[i]))
+                    inds[i] = random.sample(range(dims[i]), randround(rates[i] * dims[i]))
                     inds[i].sort()
                 ptrs[i] = 0
                 ptrs[i-1] += 1
     
-    print('100%% completed, %d generated, %.1f MiB written.' % (written, f.tell() / 1048576.0))
+    print('100%% completed, %d generated, %s written.' % (written, human_size(f.tell())))
     f.close()
     print('Successfully written into %s.' % output)
     return 0
