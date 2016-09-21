b118e945dc7832dfec4df15a2e1b945b3fd66fbd
Star Brilliant <m13253@hotmail.com>
2016-09-22 00:20:07 +0800

    Extract kernel for MATLAB

---
+++ matlab/ttm.cu
@@ -0,0 +1,42 @@
+typedef double sptScalar;
+
+__global__ void spt_TTMKernel(
+    sptScalar *Y_val, size_t Y_stride, size_t Y_nnz,
+    const sptScalar *X_val, size_t X_nnz, size_t *X_inds_m,
+    size_t *fiberidx_val, size_t fiberidx_len,
+    const sptScalar *U_val, size_t U_nrows, size_t U_ncols, size_t U_stride,
+    size_t block_offset
+) {
+    extern __shared__ sptScalar mem_pool[];
+
+    const size_t tidx = threadIdx.x;
+    const size_t tidy = threadIdx.y;
+    const size_t i = (blockIdx.x + block_offset) * blockDim.x + tidx;
+    //const size_t off = blockIdx.x * blockDim.x + tidx;
+    size_t inz_begin, inz_end;
+    if(i < Y_nnz) {
+        inz_begin = fiberidx_val[i];
+        inz_end = fiberidx_val[i+1];
+    }
+    __syncthreads();
+
+    //sptScalar * const Y_shr = (sptScalar *) &mem_pool[tidx*Y_stride]; // size U_ncols
+    sptScalar * const Y_shr = (sptScalar *) mem_pool; // size U_ncols
+    if(i < Y_nnz && tidy < U_ncols) {
+        Y_shr[tidx * Y_stride + tidy] = 0;
+    }
+    __syncthreads();
+
+    if(i < Y_nnz && tidy < U_ncols) {
+        for(size_t j = inz_begin; j < inz_end; ++j) {
+            const size_t r = X_inds_m[j];
+            Y_shr[tidx * Y_stride + tidy] += X_val[j] * U_val[r*U_stride + tidy];
+        }
+    }
+    __syncthreads();
+
+    if(i < Y_nnz && tidy < U_ncols) {
+        Y_val[i*Y_stride + tidy] = Y_shr[tidx*Y_stride + tidy];
+    }
+    __syncthreads();
+}
---
+++ matlab/ttm_naive.cu
@@ -0,0 +1,23 @@
+typedef double sptScalar;
+
+__global__ void spt_TTMNaiveKernel(
+    sptScalar *Y_val, size_t Y_stride, size_t Y_nnz,
+    const sptScalar *X_val, size_t X_nnz, size_t *X_inds_m,
+    size_t *fiberidx_val, size_t fiberidx_len,
+    const sptScalar *U_val, size_t U_nrows, size_t U_ncols, size_t U_stride,
+    size_t block_offset
+) {
+    const size_t tidx = threadIdx.x;
+    const size_t tidy = threadIdx.y;
+    const size_t i = (blockIdx.x + block_offset) * blockDim.x + tidx;
+
+    if(i >= Y_nnz || tidy >= U_ncols) return;
+    const size_t inz_begin = fiberidx_val[i];
+    const size_t inz_end = fiberidx_val[i+1];
+
+    Y_val[i*Y_stride + tidy] = 0;
+    for(size_t j = inz_begin; j < inz_end; ++j) {
+        const size_t r = X_inds_m[j];
+        Y_val[i*Y_stride + tidy] += X_val[j] * U_val[r*U_stride + tidy];
+    }
+}
