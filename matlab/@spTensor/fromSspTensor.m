d1144ef9c03a6094c3b8fce4b3499a9405c67a6b
Star Brilliant <m13253@hotmail.com>
2016-09-28 01:53:46 +0800

    Conversion function

---
+++ matlab/@spTensor/fromSspTensor.m
@@ -0,0 +1,16 @@
+function tsr = fromSspTensor(src, epsilon)
+    tsr = spTensor(src.ndims);
+    for i = 1:src.nnz
+        for j = 1:src.ndims(src.mode)
+            data = src.values(i, j);
+            if isnan(data) || isinf(data) ||
+                ~(data < epsilon && data > -epsilon)
+                tsr.inds(:, tsr.nnz+1) = src.inds(:, i);
+                tsr.inds(src.mode, tsr.nnz+1) = j;
+                tsr.values(tsr.nnz+1, 1) = data;
+                tsr.nnz = tsr.nnz + 1;
+            end
+        end
+    end
+    tsr = tsr.sort();
+end
