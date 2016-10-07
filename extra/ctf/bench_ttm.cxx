98e0f3884321214aa05f028b2755336c3f96f27d
Star Brilliant <m13253@hotmail.com>
2016-10-07 21:08:54 +0800

    CTF benchmark for comparison

--- extra/ctf/bench_ttm.cxx
+++ extra/ctf/bench_ttm.cxx
@@ -13,6 +13,7 @@
 #include <math.h>
 #include <assert.h>
 #include <algorithm>
+#include <vector>
 #include "../src/shared/util.h"
 #include "../src/interface/world.h"
 #include "../src/interface/tensor.h"
@@ -20,41 +21,101 @@
 
 using namespace CTF;
 
+CTF_Tensor *make_prod(
+    bool is_sparse,
+    CTF_World &dw,
+    std::string const & name,
+    std::vector<int>  ndimsA,
+    std::vector<int> &ndimsB,
+    int  mode
+) {
+    int nmodes = ndimsA.size();
+    ndimsA[mode] = ndimsB[1];
+    std::vector<int> ns(nmodes, NS);
+    CTF_Tensor *result = new CTF_Tensor(nmodes, is_sparse, ndimsA.data(), ns.data(), dw, Ring<double>(), name.c_str(), true);
+    return result;
+}
+
+CTF_Tensor *make_B(
+    CTF_World &dw,
+    std::string const & name,
+    std::vector<int> const &ndimsA,
+    std::vector<int> &ndimsB,
+    int mode,
+    int sizeB
+) {
+    ndimsB.resize(2);
+    ndimsB[0] = ndimsA[mode];
+    ndimsB[1] = sizeB;
+    std::vector<int> ns(2, NS);
+    CTF_Tensor *result = new CTF_Tensor(2, false, ndimsB.data(), ns.data(), dw, Ring<double>(), name.c_str(), true);
+    return result;
+}
+
+CTF_Tensor *read_tensor(
+    std::string const & filename,
+    bool is_sparse,
+    CTF_World &dw,
+    std::string const & name,
+    std::vector<int> &ndims
+) {
+    FILE *f = fopen(filename.c_str(), "r");
+    int nmodes;
+    fscanf(f, "%d", &nmodes);
+    ndims.resize(nmodes);
+    for(int i = 0; i < nmodes; ++i) {
+        fscanf(f, "%d", &ndims[i]);
+    }
+    std::vector<int> ns(nmodes, NS);
+    CTF_Tensor *result = new CTF_Tensor(nmodes, is_sparse, ndims.data(), ns.data(), dw, Ring<double>(), name.c_str(), true);
+    std::vector<int64_t> inds;
+    std::vector<double> values;
+    for(;;) {
+        for(int i = 0; i < nmodes; ++i) {
+            long long t;
+            if(fscanf(f, "%lld", &t) == 1) {
+                inds.push_back(t);
+            } else {
+                goto read_done;
+            }
+        }
+        double v;
+        if(fscanf(f, "%lf", &v) == 1) {
+            values.push_back(v);
+        } else {
+            goto read_done;
+        }
+    }
+read_done:
+    result->read(values.size(), inds.data(), values.data());
+    fclose(f);
+    return result;
+}
+
 int bench_contraction(
                       int          niter,
                       int          mode,
-                      std::string  filename,
-                      CTF_World   &dw){
+                      std::string const & filenamea,
+                      std::string const & filenameb,
+                      CTF_World   &dw
+                 ){
 
   int rank, i, num_pes;
 
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &num_pes);
 
-  int n_A[3] = {0, 1, 2};
-  int n_B[2] = {0, 1};
-  int n_C[3] = {0, 1, 2};
-  int NS_[3] = {NS, NS, NS};
-  const char *iB = "01";
-  const char *iAC;
-  switch(mode) {
-  case 0:
-    iAC = "120";
-    break;
-  case 1:
-    iAC = "021";
-    break;
-  case 2:
-    iAC = "012";
-    break;
-  default:
-    assert(0);
-  }
+  char iA[] = "012";
+  char iB[] = "-3";
+  char iC[] = "012";
+  iB[0] = iA[mode];
+  iC[mode] = '3';
 
-  //* Creates distributed tensors initialized with zeros
-  CTF_Tensor A(3, true, n_A, NS_, dw, Ring<double>(), "A", true);
-  CTF_Tensor B(2, true, n_B, NS_, dw, Ring<double>(), "B", true);
-  CTF_Tensor C(3, true, n_C, NS_, dw, Ring<double>(), "C", true);
+  std::vector<int> ndimsA, ndimsB;
+  CTF_Tensor *A = read_tensor(filenamea, true, dw, "A", ndimsA);
+  CTF_Tensor *B = make_B(dw, "B", ndimsA, ndimsB, mode, 16);
+  B->fill_random(-1, 1);
+  CTF_Tensor *C = make_prod(true, dw, "C", ndimsA, ndimsB, mode);
 
   //////////////////////////////////////////////////
   // TODO: read tensor from "filename" into A, B  //
@@ -63,13 +124,19 @@ int bench_contraction(
   double st_time = MPI_Wtime();
 
   for (i=0; i<niter; i++){
-    C[iAC] += A[iAC]*B[iB];
+    (*C)[iC] = (*A)[iA]*(*B)[iB];
   }
 
   double end_time = MPI_Wtime();
 
     printf("Performed %d iterations of mode %d in %lf sec/iter\n",
            niter, mode, (end_time-st_time)/niter);
+
+  C->print();
+
+  delete C;
+  delete B;
+  delete A;
 
   return 1;
 }
@@ -86,43 +153,32 @@ char* getCmdOption(char ** begin,
 
 
 int main(int argc, char ** argv){
-  int rank, np, niter, n;
+  int rank, np, niter;
   int const in_num = argc;
   char ** input_str = argv;
   char const * A;
   char const * B;
-  char const * C;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &np);
 
-  if (getCmdOption(input_str, input_str+in_num, "-n")){
-    n = atoi(getCmdOption(input_str, input_str+in_num, "-n"));
-    if (n < 0) n = 4;
-  } else n = 4;
-
   if (getCmdOption(input_str, input_str+in_num, "-niter")){
     niter = atoi(getCmdOption(input_str, input_str+in_num, "-niter"));
-    if (niter < 0) niter = 3;
-  } else niter = 3;
+  } else niter = 1000;
 
   if (getCmdOption(input_str, input_str+in_num, "-A")){
     A = getCmdOption(input_str, input_str+in_num, "-A");
-  } else A = "ik";
+  } else A = "a.tns";
   if (getCmdOption(input_str, input_str+in_num, "-B")){
     B = getCmdOption(input_str, input_str+in_num, "-B");
-  } else B = "kj";
-  if (getCmdOption(input_str, input_str+in_num, "-C")){
-    C = getCmdOption(input_str, input_str+in_num, "-C");
-  } else C = "ij";
-
+  } else B = "b.tns";
 
 
   {
     CTF_World dw(argc, argv);
     for(int mode = 0; mode < 3; ++mode) {
-      bench_contraction(niter, mode, "", dw);
+      bench_contraction(niter, mode, A, B, dw);
     }
   }
 
