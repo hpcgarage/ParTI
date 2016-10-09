59c40a4c5e2349b919528d2975ca03b5797871d1
jli <fruitfly1026@gmail.com>
2016-10-08 20:45:36 -0400

    ctf update

--- extra/ctf/build.sh
+++ extra/ctf/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mpicxx -std=c++0x -std=c++0x -fopenmp -Wall  -D_POSIX_C_SOURCE=200112L -D__STDC_LIMIT_MACROS -DFTN_UNDERSCORE=1   bench_ttm.cxx -o ../bin/bench_ttm -I../include/ -L../lib -lctf -lblas -g
+mpiicpc -std=c++0x -qopenmp -O3 -Wall  -D_POSIX_C_SOURCE=200112L -D__STDC_LIMIT_MACROS -DUSE_SP_MKL=1 -DFTN_UNDERSCORE=1   bench_ttm.cxx -o bench_ttm -I../include/ -L/home/jli/Software/ctf-1.4.1/lib -lctf -mkl
 
