3ced75b32e5482547bbf1dec2dac49346e62ffd0
Star Brilliant <m13253@hotmail.com>
2016-11-08 01:05:15 +0800

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+echo "This script will do an out-of-tree build of SpTOL into the 'build' directory."
+
+mkdir -p build
+cd build
+
+cmake ..
+# Use this if you have GCC >= 6.0 and CUDA <= 8.0
+#cmake .. -DCMAKE_C_COMPILER=gcc-5
+
+make
+
+echo "Finished. Check the 'build' directory for results."
