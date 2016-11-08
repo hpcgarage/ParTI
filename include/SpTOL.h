0117a33023003795bd9298f179437b342b50bf99
Star Brilliant <m13253@hotmail.com>
2016-11-08 23:27:54 +0800

    Remove unused sptCudaSparseTensorMulMatrixNew

--- include/SpTOL.h
+++ include/SpTOL.h
@@ -219,7 +219,6 @@ int sptSparseTensorDotDiv(sptSparseTensor *Z, const sptSparseTensor *X, const sp
 int sptSparseTensorMulMatrix(sptSemiSparseTensor *Y, sptSparseTensor *X, const sptMatrix *U, size_t mode);
 int sptOmpSparseTensorMulMatrix(sptSemiSparseTensor *Y, sptSparseTensor *X, const sptMatrix *U, size_t mode);
 int sptCudaSparseTensorMulMatrix(sptSemiSparseTensor *Y, sptSparseTensor *X, const sptMatrix *U, size_t mode);
-int sptCudaSparseTensorMulMatrixNew(sptSemiSparseTensor *Y, sptSparseTensor *X, const sptMatrix *U, size_t mode);
 
 /**
  * Kronecker product
