1308203675eea478403ab9250dbbdadca8e3b0ec
Star Brilliant <m13253@hotmail.com>
2016-11-27 01:08:05 +0800

    cast.h

--- matlab/utility/cast.h
+++ matlab/utility/cast.h
@@ -1,67 +1,67 @@
 #include "matrix.h"
 
-#define DefineCastArray(funcname, T)                    \
-T *funcname(const mxArray *pm) {	                \
-    mxClassID clsid = mxGetClassID(pm);	                \
-    void *data = mxGetData(pm);	                        \
-    size_t n = mxGetNumberOfElements(pm);	        \
-    size_t i;	                                        \
-    T *result = malloc(n * sizeof (T));	                \
-    switch(clsid) {	                                \
-    case mxINT8_CLASS:	                                \
-        for(i = 0; i < n; ++i) {	                \
-            result[i] = i[(signed char *) data];	\
-        }	                                        \
-        break;	                                        \
-    case mxUINT8_CLASS:	                                \
-        for(i = 0; i < n; ++i) {	                \
-            result[i] = i[(unsigned char *) data];	\
-        }	                                        \
-        break;	                                        \
-    case mxINT16_CLASS:	                                \
-        for(i = 0; i < n; ++i) {	                \
-            result[i] = i[(short *) data];	        \
-        }	                                        \
-        break;	                                        \
-    case mxUINT16_CLASS:	                        \
-        for(i = 0; i < n; ++i) {	                \
-            result[i] = i[(unsigned short *) data];	\
-        }	                                        \
-        break;	                                        \
-    case mxINT32_CLASS:	                                \
-        for(i = 0; i < n; ++i) {	                \
-            result[i] = i[(int *) data];	        \
-        }	                                        \
-        break;	                                        \
-    case mxUINT32_CLASS:	                        \
-        for(i = 0; i < n; ++i) {	                \
-            result[i] = i[(unsigned int *) data];	\
-        }	                                        \
-        break;	                                        \
-    case mxINT64_CLASS:	                                \
-        for(i = 0; i < n; ++i) {	                \
-            result[i] = i[(long long *) data];	        \
-        }	                                        \
-        break;	                                        \
-    case mxUINT64_CLASS:	                        \
-        for(i = 0; i < n; ++i) {	                \
-            result[i] = i[(unsigned long long *) data];	\
-        }	                                        \
-        break;	                                        \
-    case mxSINGLE_CLASS:	                        \
-        for(i = 0; i < n; ++i) {	                \
-            result[i] = i[(float *) data];	        \
-        }	                                        \
-        break;	                                        \
-    case mxDOUBLE_CLASS:	                        \
-        for(i = 0; i < n; ++i) {	                \
-            result[i] = i[(double *) data];	        \
-        }	                                        \
-        break;	                                        \
-    default:	                                        \
-        free(result);	                                \
-        return NULL;	                                \
-    }	                                                \
-    return result;                                      
+#define DefineCastArray(funcname, T)                            \
+T *funcname(const mxArray *pm) {                                \
+    mxClassID clsid = mxGetClassID(pm);                         \
+    void *data = mxGetData(pm);                                 \
+    size_t n = mxGetNumberOfElements(pm);                       \
+    size_t i;                                                   \
+    T *result = malloc(n * sizeof (T));                         \
+    switch(clsid) {                                             \
+    case mxINT8_CLASS:                                          \
+        for(i = 0; i < n; ++i) {                                \
+            result[i] = (T) ((signed char *) data)[i];          \
+        }                                                       \
+        break;                                                  \
+    case mxUINT8_CLASS:                                         \
+        for(i = 0; i < n; ++i) {                                \
+            result[i] = (T) ((unsigned char *) data)[i];        \
+        }                                                       \
+        break;                                                  \
+    case mxINT16_CLASS:                                         \
+        for(i = 0; i < n; ++i) {                                \
+            result[i] = (T) ((short *) data)[i];                \
+        }                                                       \
+        break;                                                  \
+    case mxUINT16_CLASS:                                        \
+        for(i = 0; i < n; ++i) {                                \
+            result[i] = (T) ((unsigned short *) data)[i];       \
+        }                                                       \
+        break;                                                  \
+    case mxINT32_CLASS:                                         \
+        for(i = 0; i < n; ++i) {                                \
+            result[i] = (T) ((int *) data)[i];                  \
+        }                                                       \
+        break;                                                  \
+    case mxUINT32_CLASS:                                        \
+        for(i = 0; i < n; ++i) {                                \
+            result[i] = (T) ((unsigned int *) data)[i];         \
+        }                                                       \
+        break;                                                  \
+    case mxINT64_CLASS:                                         \
+        for(i = 0; i < n; ++i) {                                \
+            result[i] = (T) ((long long *) data)[i];            \
+        }                                                       \
+        break;                                                  \
+    case mxUINT64_CLASS:                                        \
+        for(i = 0; i < n; ++i) {                                \
+            result[i] = (T) ((unsigned long long *) data)[i];   \
+        }                                                       \
+        break;                                                  \
+    case mxSINGLE_CLASS:                                        \
+        for(i = 0; i < n; ++i) {                                \
+            result[i] = (T) ((float *) data)[i];                \
+        }                                                       \
+        break;                                                  \
+    case mxDOUBLE_CLASS:                                        \
+        for(i = 0; i < n; ++i) {                                \
+            result[i] = (T) ((double *) data)[i];               \
+        }                                                       \
+        break;                                                  \
+    default:                                                    \
+        free(result);                                           \
+        return NULL;                                            \
+    }                                                           \
+    return result;                                              \
 }
 
