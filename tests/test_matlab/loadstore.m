9d21ef65211101de4ed976badcf85722a403166a
Star Brilliant <m13253@hotmail.com>
2016-12-01 00:49:00 +0800

    Add some new tests

--- tests/test_matlab/loadstore.m
+++ tests/test_matlab/loadstore.m
@@ -2,5 +2,5 @@ ifile = '../tensors/3d_3_6.tns';
 ofile = '/tmp/3d_3_6.tns';
 
 tns = sptLoadSparseTensor(1, ifile);
-tns.sptDumpSparseTensor(1, ofile);
+sptDumpSparseTensor(tns, 1, ofile);
 sptFreeSparseTensor(tns);
