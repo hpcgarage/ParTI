cf986fd1f129859b3e769eacaabd2accfc31f3f9
Star Brilliant <m13253@hotmail.com>
2016-12-01 23:46:07 +0800

    Fix linking to cBLAS

--- src/sptensor/cpd.c
+++ src/sptensor/cpd.c
@@ -18,14 +18,7 @@
 
 #include <SpTOL.h>
 #include "sptensor.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-    #include <cblas.h>
-#ifdef __cplusplus
-}
-#endif
+#include <cblas.h>
 
 
 double CpdAlsStep(
