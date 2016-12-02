70acbfea294ffbefe9c7019434917f7cbf5657d2
Star Brilliant <m13253@hotmail.com>
2016-12-02 17:09:45 +0800

    Update build system

--- build.sh
+++ build.sh
@@ -1,15 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 echo "This script will do an out-of-tree build of ParTI into the 'build' directory."
 
+# If you have GCC >= 6.0 and CUDA <= 8.0,
+# write this into 'build.config': -DCMAKE_C_COMPILER=gcc-5
+# You can also write other configuation flags into 'build.config'
+declare -a CMAKE_FLAGS
+[ -e build.config ] && CMAKE_FLAGS=("${CMAKE_FLAGS[@]}" $(<build.config))
+CMAKE_FLAGS=("${CMAKE_FLAGS[@]}" "$@")
+
 mkdir -p build
 cd build
 
-cmake .. "$@"
-# Use this if you have GCC >= 6.0 and CUDA <= 8.0
-#cmake .. -DCMAKE_C_COMPILER=gcc-5
+cmake "${CMAKE_FLAGS[@]}" ..
 
 make
 
