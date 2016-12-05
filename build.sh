a549a296a02efcf79c4c63da37eff8d79f6aac26
Star Brilliant <m13253@hotmail.com>
2016-12-05 22:10:46 +0800

    CMAKE_C_COMPILER -> CUDA_HOST_COMPILER

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 echo "This script will do an out-of-tree build of ParTI into the 'build' directory."
 
 # If you have GCC >= 6.0 and CUDA <= 8.0,
-# write this into 'build.config': -DCMAKE_C_COMPILER=gcc-5
+# write this into 'build.config': -DCUDA_HOST_COMPILER=gcc-5
 # You can also write other configuation flags into 'build.config'
 declare -a CMAKE_FLAGS
 [ -e build.config ] && CMAKE_FLAGS=("${CMAKE_FLAGS[@]}" $(<build.config))
