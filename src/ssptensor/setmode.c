620c27456630c85cea98871491733f45ea8d3d3e
Star Brilliant <m13253@hotmail.com>
2017-04-11 00:36:31 +0800

    ssp setmode

---
+++ src/ssptensor/setmode.c
@@ -0,0 +1,37 @@
+/*
+    This file is part of ParTI!.
+
+    ParTI! is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3 of
+    the License, or (at your option) any later version.
+
+    ParTI! is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with ParTI!.
+    If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <ParTI.h>
+#include "ssptensor.h"
+#include "../error/error.h"
+
+int spt_SemiSparseTensorSetMode(
+    sptSemiSparseTensor       *dest,
+    const sptSemiSparseTensor *src,
+    size_t                    newmode
+) {
+    int result = 0;
+    /* Something like this, but better */
+    sptSparseTensor tmp;
+    result = sptSemiSparseTensorToSparseTensor(&tmp, src, 0);
+    spt_CheckError(result, "ssp setmode", NULL);
+    sptSparseTensorToSemiSparseTensor(dest, &tmp, newmode);
+    spt_CheckError(result, "ssp setmode", NULL);
+
+    return 0;
+}
