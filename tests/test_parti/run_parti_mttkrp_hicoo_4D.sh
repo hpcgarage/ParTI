c156d26d20208dedfcbc7032d197311b7bbbddc5
Jiajia Li <fruitfly1026@gmail.com>
2018-01-06 22:51:26 -0500

    sync

--- tests/test_parti/run_parti_mttkrp_hicoo_4D.sh
+++ tests/test_parti/run_parti_mttkrp_hicoo_4D.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 declare -a array=("one" "two" "three")
-declare -a s3tsrs=("choa700k" "1998DARPA" "nell2" "freebase_music" "freebase_sampled" "nell1" "delicious")
-declare -a l3tsrs=("amazon-reviews" "patents" "reddit-2015")
-declare -a sl4tsrs=("flickr-4d" "enron-4d" "nips-4d" "delicious-4d")
-declare -a test_tsr_names=("amazon-reviews")
-declare -a threads=("16" "24")
+declare -a sl4tsrs=("flickr-4d" "enron-4d" "nips-4d")
+declare -a test_tsr_names=("delicious-4d")
+declare -a threads=("24")
 # declare -a sk_range=("8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20")
-declare -a sk_range=("12" "13" "14" "15" "16" "17" "18" "19" "20")
+declare -a sk_range=("8" "10" "12" "14" "16" "18" "20")
 
 tsr_path="/scratch/jli458/BIGTENSORS"
 out_path="timing_parti/hicoo/uint-fast8-simd-fulltest"
 nthreads=32
-nmodes=3
+nmodes=4
 modes="$(seq -s ' ' 0 $((nmodes-1)))"
 impl_num=25
 
@@ -34,14 +32,17 @@ tk=8
 # for R in 8 16 32 64
 for R in 16
 do
-	for tsr_name in "${l3tsrs[@]}"
+	for tsr_name in "${sl4tsrs[@]}"
 	do
 
-		if [ ${tsr_name} = "amazon-reviews" ] || [ ${tsr_name} = "reddit-2015" ]; then
-			sk_range=("15" "17")
+		if [ ${tsr_name} = "enron-4d" ]; then
+			sk_range=("8" "9")
 		fi
-		if [ ${tsr_name} = "patents" ]; then
-			sk_range=("11" "13")
+		if [ ${tsr_name} = "flickr-4d" ]; then
+			sk_range=("12" "14")
+		fi
+		if [ ${tsr_name} = "nips-4d" ]; then
+			sk_range=("8" "9")
 		fi
 
 		for mode in ${modes[@]}
@@ -67,7 +68,6 @@ do
 					./build/tests/mttkrp_hicoo_matrixtiling -i ${tsr_path}/${tsr_name}.tns -b ${sb} -k ${sk} -c ${sc} -m ${mode} -d ${dev_id} -r ${R} -t ${tk} -h ${tb} > ${out_path}/${tsr_name}-b${sb}-k${sk}-c${sc}-m${mode}-r${R}-tk${tk}-tb${tb}.txt
 				done
 			done
-		# for ((tb=1; tb <= ((${nthreads}/${tk})) ; tb=tb*2))
 
 		done
 	done
