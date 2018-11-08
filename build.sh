887f507ef3137f9f0a68f2cc06f4361cc303cda6
Star Brilliant <coder@poorlab.com>
2018-11-09 01:02:29 +0800

    Recover some work

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ echo "This script will do an out-of-tree build of ParTI into the 'build' directo
 
 # If you have GCC >= 6.0 and CUDA <= 8.0,
 # write this into 'build.config': -DCUDA_HOST_COMPILER=gcc-5
+# If you want to use alternate compilers (e.g. Intel C++ Compilers),
+# write this into 'build.config':
+# -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc -DUSE_ICC=ON
 # You can also write other configuation flags into 'build.config'
 declare -a CMAKE_FLAGS
 [ -e build.config ] && CMAKE_FLAGS=("${CMAKE_FLAGS[@]}" $(<build.config))
@@ -14,9 +17,7 @@ CMAKE_FLAGS=("${CMAKE_FLAGS[@]}" "$@")
 mkdir -p build
 cd build
 
-# CC=icc CXX=icpc cmake "${CMAKE_FLAGS[@]}" ..
-# CC=gcc CXX=g++ cmake "${CMAKE_FLAGS[@]}" ..
-cmake "${CMAKE_FLAGS[@]}" .. 		# Use Default cc and CC
+cmake "${CMAKE_FLAGS[@]}" ..
 
 make
 
