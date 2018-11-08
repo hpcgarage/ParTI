e50ed21ff52d84a1d880e007564b6f852f65925c
Star Brilliant <coder@poorlab.com>
2018-11-09 01:40:12 +0800

    Add test.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-echo "This script will do an out-of-tree build of ParTI into the 'build' directory."
+echo "This script will do an out-of-tree build of ParTI into the 'build' directory by calling CMake."
+echo
+echo "To make things easier, you can write your personalized CMake options in build.config and it will be passed to CMake."
+echo
+echo "If you have GCC >= 6.0 and CUDA <= 8.0, write this into 'build.config': -DCUDA_HOST_COMPILER=gcc-5"
+echo "If you want to use alternate compilers (e.g. Intel C++ Compilers), write this into 'build.config': -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc -DUSE_ICC=ON "
+echo "================================================================================"
+echo
 
-# If you have GCC >= 6.0 and CUDA <= 8.0,
-# write this into 'build.config': -DCUDA_HOST_COMPILER=gcc-5
-# If you want to use alternate compilers (e.g. Intel C++ Compilers),
-# write this into 'build.config':
-# -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc -DUSE_ICC=ON
-# You can also write other configuation flags into 'build.config'
 declare -a CMAKE_FLAGS
 [ -e build.config ] && CMAKE_FLAGS=("${CMAKE_FLAGS[@]}" $(<build.config))
 CMAKE_FLAGS=("${CMAKE_FLAGS[@]}" "$@")
